6c0ddb925e208be816e719c7a03f32762a04f9c3
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-02-09 20:48:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,30 @@ import time
 import colorama
 from colorama import Fore
 
-token = input("Token: ")
-channelId = input("Channel Id: ")
-userId = input("User Id: ")
+def deleteAllMessage():
+    token = input("Token: ")
+    channelId = input("Channel Id: ")
+    userId = input("User Id: ")
 
-headers = {"authorization": token}
+    headers = {"authorization": token}
 
-sm = requests.get(f"https://discord.com/api/v9/channels/{channelId}/messages/search?author_id={userId}", headers=headers)
+    sm = requests.get(f"https://discord.com/api/v9/channels/{channelId}/messages/search?author_id={userId}", headers=headers, timeout=1000)
+    try:
+        mlist = sm.json().get("messages")
+        for item in mlist:
+            messageId = item[0].get('id')
+            mcontent = item[0].get('content')
+            dm = requests.delete(f'https://discord.com/api/v9/channels/{channelId}/messages/{messageId}', headers=headers)
+            if dm.status_code == 204:
+                print(Fore.GREEN + f"Message Deleted: {mcontent}")
+            else:
+                print(Fore.RED + f"Error when delete message: {dm.status_code}")
+                if dm.status_code == 429:
+                    print(Fore.YELLOW + "Rate Limit!")
+                    time.sleep(1)
+        print(Fore.BLUE + "Done!")
+    except:
+        print("Failed to get messages, retrying...")
+        deleteAllMessage()
 
-try:
-    mlist = sm.json().get("messages")
-    for item in mlist:
-        messageId = item[0].get('id')
-        mcontent = item[0].get('content')
-        dm = requests.delete(f'https://discord.com/api/v9/channels/{channelId}/messages/{messageId}', headers=headers)
-        if dm.status_code == 204:
-            print(Fore.GREEN + f"Message Deleted: {mcontent}")
-        else:
-            print(Fore.RED + f"Error when delete message: {dm.status_code}")
-        time.sleep(1)
-    print(Fore.BLUE + "Done!")
-except:
-    print("Failed to get messages!")
+deleteAllMessage()
